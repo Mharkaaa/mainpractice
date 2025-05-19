a4084a6b87e18c39cfdcea11f238bf911e8f7f45
Mharkaaa <121071461+Mharkaaa@users.noreply.github.com>
2025-05-19 17:03:57 +0800

    Update main.spec.js

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -22,16 +22,6 @@ test('testmain', async ({ page }) => {
   await page.locator('[data-test="back-to-products"]').click();
 
 
-     // Ensure dropdown is in the DOM and visible before interacting
-  const dropdown = page.locator('[aria-label="Choose a Name (A to Z)"]');
-  
-  // Wait for the dropdown to be visible before interacting
-  await dropdown.waitFor({ state: 'attached' }); // Ensures it's in the DOM first
-  await dropdown.waitFor({ state: 'visible' }); // Wait for it to be visible
-  
-  // Select the 'Price (low to high)' option
-  await dropdown.selectOption({ label: 'Price (low to high)' });
-
-});
+})
 
 
